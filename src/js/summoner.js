931440e11dc6035a98ff33e1e066f7c15472b595
Christophe Loeys <christophe.loeys@gmail.com>
2017-10-23 16:36:38 +0200

    champs of recently played matches listed

--- src/js/summoner.js
+++ src/js/summoner.js
@@ -1,4 +1,3 @@
-
 require('request').debug = true;
 var fs = require('fs');
 
@@ -8,14 +7,12 @@ const removeChildren = (node) => {
     }
 };
 
-const retrieveChampion = async(kayn, id, fullName = false) => {
-    let champion = await new Promise((resolve, reject) => kayn.Static.Champion.get(id).callback(data => resolve(data)));
-    console.log(champion);
-    let champName = fullName ? `${champion.name} ${champion.title}` : champion.name;
-    return new Promise((resolve, reject) => resolve(champName));
+const formatChamp = (champion, fullName = false) => {
+    return fullName ? `${champion.name} ${champion.title}` : champion.name;
 };
 
 const addMatchToTarget = (doc, target, champName) => {
+    console.log(champName);
     const match = doc.createElement('div');
     match.classList.add('match');
     const champion = doc.createElement('p');
@@ -24,8 +21,7 @@ const addMatchToTarget = (doc, target, champName) => {
     match.appendChild(champion);
 };
 
-exports.showSummoner = async(doc, kayn, summoner, target) => {
-    console.log("hey")
+exports.showSummoner = (doc, kayn, summoner, target) => {
     if (summoner == null || summoner == '') return;
     removeChildren(target);
 
@@ -34,53 +30,39 @@ exports.showSummoner = async(doc, kayn, summoner, target) => {
     let matchesList = doc.createElement('div');
     matchesList.classList.add('matchContainer');
     let summonerObject;
-    let argh = []
+    let argh = [];
     let allChamps;
-    fs.readFile('champs.json', (err, data) => {  
+    fs.readFile('champs.json', (err, data) => {
         if (err) throw err;
         allChamps = JSON.parse(data);
     });
 
     try {
-        
+
         const lala = kayn.Summoner.by.name(summoner).then(summoner => {
-                speechSynthesis.innerHTML = summoner.name;
+                if (p.innerHTML == '') p.innerHTML = summoner.name;
                 return kayn.Matchlist.Recent.by
                     .accountID(summoner.accountId);
             }, err => console.log(err))
             .then(matchlist => {
-                return matchlist.matches.map(match => {
-                    let e = kayn.Champion.get(match.champion)
-                    return e;
-                })
+                let hello = matchlist.matches.map(match => {
+                    return match.champion;
+                });
+                return hello;
             }, err => console.log(err))
             .then(requestArray => {
-                console.log(requestArray)
                 for (let r in requestArray) {
-                    let req = requestArray[r]
-                    console.log(req)
-                    req.callback((_, data) => {
-                        let champObj = data;
-                        console.log(allChamps.find(el => el.id == champObj.id));
-                    });
+                    let req = requestArray[r];
+                    let champObj = allChamps.find(el => el.id == req);
+                    let c = formatChamp(champObj);
+                    addMatchToTarget(doc, matchesList, c);
                 }
-            })
+            });
 
     } catch (ex) {
-        console.log(ex);
-        error = ex;
+        p.innerHTML = ex;
     } finally {
-        console.log("finlly!");
-        if (error) {
-            p.innerHTML = error;
-        } else {
-            //p.innerHTML = summonerObject.name;
-        }
-
         target.appendChild(p);
-        if (matchesList.hasChildNodes()) {
-            target.appendChild(matchesList);
-
-        }
+        target.appendChild(matchesList);
     }
 };
